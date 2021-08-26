0ad6ed49a5842aa932ae39967cb4a4c14db80e4a
onoja64 <corneliussamuel64@gmail.com>
2021-08-26 11:19:41 +0100

    Commit1

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,48 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "https://boomcake.finance",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "/",
+      },
+      {
+        label: "Liquidity",
+        href: "https://boomcake.finance/liquidity",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/boomcaketokenbsc",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/boomcakebsctoken",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/boomcaketoken",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
---
+++ src/widgets/Menu/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as HomeIcon } from "./Home";
+export { default as LanguageIcon } from "./Language";
+export { default as LogoIcon } from "./Logo";
+export { default as TelegramIcon } from "./Telegram";
+export { default as TicketIcon } from "./Ticket";
+export { default as TradeIcon } from "./Trade";
+export { default as TwitterIcon } from "./Twitter";
